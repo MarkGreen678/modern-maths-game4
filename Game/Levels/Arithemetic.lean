758377ada9081d18f023c9cf73e12b6128ffbca7
MarkGreen678 <146335635+MarkGreen678@users.noreply.github.com>
2024-07-15 07:07:03 +0000

    my changes

---
+++ Game/Levels/Arithemetic.lean
@@ -0,0 +1,11 @@
+import Game.Levels.Arithemetic.L01_Commutativity.lean
+
+World "Arithemetic"
+Title "Arithemetic"
+
+Introduction
+"
+We prove simple statements using arithemetic. Essentially, what we are saying is
+...computers are good at these calculations and won't let you make silly mistakes!
+
+Click on \"Start\" below to begin."
