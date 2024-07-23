8b0e4a5b300a5da1a979c4d53b25c05a6a195765
MarkGreen678 <146335635+MarkGreen678@users.noreply.github.com>
2024-07-23 08:53:45 +0000

    Transitivity_of_subset changes

--- Game/Levels/ProofFundamentals/L01_Transitivity_of_Subset.lean
+++ Game/Levels/ProofFundamentals/L01_Transitivity_of_Subset.lean
@@ -1,24 +1,35 @@
 import GameServer.Commands
+import Mathlib.Init.Set
 
 World "Proof Fundamentals"
 Level 1
 Title "Transitivity of Subset"
 
 Introduction "
+
 Hello! We'll show that given sets 𝑆,𝑇, and 𝑈 on a type 𝑋,
- given h₁: 𝑆⊆𝑇 and h₂: 𝑇⊆𝑈, 𝑆⊆𝑈 "
+given h₁: 𝑆⊆𝑇 and h₂: 𝑇⊆𝑈, 𝑆⊆𝑈 "
+
+variable {X : Type}
+
+namespace hidden
 
 /-- given sets 𝑆,𝑇, and 𝑈 on a type 𝑋,
+
  given h₁: 𝑆⊆𝑇 and h₂: 𝑇⊆𝑈, 𝑆⊆𝑈
+
 --/
-theorem subset_trans {S T U : set X} (h₁ : S ⊆ T) (h₂ : T ⊆ U) : S ⊆ U :=  by
+
+theorem subset_trans {S T U : Set X} (h₁ : S ⊆ T) (h₂ : T ⊆ U) : S ⊆ U :=  by
   Template
     intros x hx
     apply h₂
     apply h₁
     exact hx
-Conclusion "
-Well done!
 
+Conclusion "
+
+Well done!
 Still don't understand? Ask [Proof Guide](https://chatgpt.com/g/g-sbJfmQ6te-proof-guide).
 "
+end hidden
